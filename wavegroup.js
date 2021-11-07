6a150e6908946f82814f97fa1631545f85872f07
Danmuse <danifabriziodmodica@gmail.com>
2021-11-07 15:56:46 -0300

    Add texts and improves

--- wavegroup.js
+++ wavegroup.js
@@ -5,9 +5,9 @@ export class WaveGroup {
     this.totalWaves = 3
     this.totalPoints = 6
     this.color = [
-      'rgba(0, 199, 235, 0.3)',
-      'rgba(0, 146, 199, 0.3)',
-      'rgba(0, 87, 158, 0.3)',
+      'rgba(0, 199, 235, 0.2)',
+      'rgba(0, 146, 199, 0.2)',
+      'rgba(0, 87, 158, 0.2)',
     ]
     this.waves = []
     for (let i = 0; i < this.totalWaves; i++) {
